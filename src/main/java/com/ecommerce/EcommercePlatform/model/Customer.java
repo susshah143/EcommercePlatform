7579ed57f519a980d32d0fe0060d48c73980a9c8
susshah143 <114939519+susshah143@users.noreply.github.com>
2022-10-03 20:14:41 -0500

    Correction

--- src/main/java/com/ecommerce/EcommercePlatform/model/Customer.java
+++ src/main/java/com/ecommerce/EcommercePlatform/model/Customer.java
@@ -8,8 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Scanner;
-
 @Entity
 @Setter
 @Getter
